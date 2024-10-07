0cef3db022ff3be193dd46de5b44d346ad38d955
Hellen Gabriele <142432658+gabiihzxps@users.noreply.github.com>
2024-10-07 12:22:10 -0300

    hbcgh

--- perguntas.js
+++ perguntas.js
@@ -1,100 +1,100 @@
 criaCartao(
         'Vestibular',
         '1.O que é o vestibular?',
-        'O vestibular é um exame utilizado por universidades no Brasil para selecionar candidatos para cursos de graduação.').
+        'O vestibular é um exame utilizado por universidades no Brasil para selecionar candidatos para cursos de graduação.')
 
         CriaCartão (
         'Vestibular',
         '2.Qual a diferença entre vestibular e ENEM?',
-        'O vestibular é um exame específico de cada universidade, enquanto o ENEM é um exame nacional que pode ser usado para ingresso em várias instituições.').
+        'O vestibular é um exame específico de cada universidade, enquanto o ENEM é um exame nacional que pode ser usado para ingresso em várias instituições.')
     
         CriaCartão (
         'Vestibular',
         '3.Como me inscrever para o vestibular?',
-        'A inscrição é feita no site da universidade ou instituição que oferece o exame. O candidato deve preencher um formulário e pagar uma taxa de inscrição.').
+        'A inscrição é feita no site da universidade ou instituição que oferece o exame. O candidato deve preencher um formulário e pagar uma taxa de inscrição.')
         
         CriaCartão (
         'Vestibular',
         '4.Quais são as matérias cobradas no vestibular?',
-        'Geralmente, as matérias incluem português, matemática, ciências humanas e ciências da natureza, além de uma língua estrangeira (inglês ou espanhol).').
+        'Geralmente, as matérias incluem português, matemática, ciências humanas e ciências da natureza, além de uma língua estrangeira (inglês ou espanhol).')
         
         CriaCartão (
         'Vestibular',
         '5.Como estudar para o vestibular?',
-        'Planejamento e organização são fundamentais. Faça um cronograma de estudos, utilize materiais de qualidade e pratique com provas anteriores.').
+        'Planejamento e organização são fundamentais. Faça um cronograma de estudos, utilize materiais de qualidade e pratique com provas anteriores.')
         
         CriaCartão (
         'Vestibular',
         '6.O que cai mais no vestibular?',
-        'Isso pode variar conforme a universidade, mas tópicos comuns incluem interpretação de texto, funções matemáticas, física, química, e história do Brasil.').
+        'Isso pode variar conforme a universidade, mas tópicos comuns incluem interpretação de texto, funções matemáticas, física, química, e história do Brasil.')
         
         CriaCartão (
         'Vestibular',
         '7.Os vestibulares são iguais em todas as universidades?',
-        'Não, cada universidade pode ter seu formato de vestibular, com número de questões e disciplinas diferentes.').
+        'Não, cada universidade pode ter seu formato de vestibular, com número de questões e disciplinas diferentes.')
         
         CriaCartão (
         'Vestibular',
         '8.Quais são os vestibulares mais concorridos do Brasil?',
-        'Os vestibulares da USP, Unicamp e UFRJ são conhecidos por serem altamente concorridos devido à qualidade das instituições.').
+        'Os vestibulares da USP, Unicamp e UFRJ são conhecidos por serem altamente concorridos devido à qualidade das instituições.')
         
         CriaCartão (
         'Vestibular',
         '9.Como funciona a nota de corte no vestibular?',
-        'A nota de corte é a menor pontuação necessária para que um candidato entre na próxima fase do processo seletivo, ou seja, esteja na lista de aprovados.').
+        'A nota de corte é a menor pontuação necessária para que um candidato entre na próxima fase do processo seletivo, ou seja, esteja na lista de aprovados.')
         
         CriaCartão (
         'Vestibular',
         '10.É possível usar a nota do ENEM no vestibular?',
-        'Sim, muitas universidades aceitam a nota do ENEM como parte do processo seletivo para o ingresso.').
+        'Sim, muitas universidades aceitam a nota do ENEM como parte do processo seletivo para o ingresso.')
         
         CriaCartão (
         'Vestibular',
         '11.Como são corrigidas as redações no vestibular?',
-        'As redações são avaliadas por critérios como coerência, coesão, gramática e desenvolvimento de argumentação.').
+        'As redações são avaliadas por critérios como coerência, coesão, gramática e desenvolvimento de argumentação.')
         
         CriaCartão (
         'Vestibular',
         '12.O que é preciso levar no dia do vestibular?',
-        'Documento de identificação com foto, canetas pretas em material transparente e, em alguns casos, a confirmação de inscrição.').
+        'Documento de identificação com foto, canetas pretas em material transparente e, em alguns casos, a confirmação de inscrição.')
         
         CriaCartão (
         'Vestibular',
         '13.Quais dicas para o dia do vestibular?',
-        'Durma bem na noite anterior, chegue cedo ao local, leia atentamente as questões e gerencie bem seu tempo durante a prova.').
+        'Durma bem na noite anterior, chegue cedo ao local, leia atentamente as questões e gerencie bem seu tempo durante a prova.')
         
         CriaCartão (
         'Vestibular',
         '14.Posso levar celular no dia do vestibular?',
-        'Normalmente, celulares são permitidos, mas devem ficar desligados e guardados durante a prova.').
+        'Normalmente, celulares são permitidos, mas devem ficar desligados e guardados durante a prova.')
         
         CriaCartão (
         'Vestibular',
         '15.Como lidar com o nervosismo no vestibular?',
-        'Pratique técnicas de respiração, faça pausas para relaxar, e visualize o sucesso para aliviar a tensão.').
+        'Pratique técnicas de respiração, faça pausas para relaxar, e visualize o sucesso para aliviar a tensão.')
         
         CriaCartão (
         'Vestibular',
         '16.Qual a validade da pontuação do vestibular?',
-        'A pontuação geralmente é válida apenas para o processo seletivo daquele ano ou semestre.').
+        'A pontuação geralmente é válida apenas para o processo seletivo daquele ano ou semestre.')
         
         CriaCartão (
         'Vestibular',
         '17.Há limite de idade para fazer o vestibular?',
-        'Não, desde que o candidato tenha concluído o ensino médio ou equivalente, não há limite de idade.').
+        'Não, desde que o candidato tenha concluído o ensino médio ou equivalente, não há limite de idade.')
         
         CriaCartão (
         'Vestibular',
         '18.Posso fazer o vestibular mais de uma vez?',
-        'Sim, você pode tentar quantas vezes desejar para melhorar sua pontuação ou tentar outros cursos.').
+        'Sim, você pode tentar quantas vezes desejar para melhorar sua pontuação ou tentar outros cursos.')
         
         CriaCartão (
         'Vestibular',
         '19.Quais são os erros mais comuns no vestibular?',
-        'Erros comuns incluem falta de atenção às questões, má administração do tempo, e não revisar as respostas.').
+        'Erros comuns incluem falta de atenção às questões, má administração do tempo, e não revisar as respostas.')
         
         CriaCartão (
         'Vestibular',
         '20.O que fazer se não passar no vestibular?',
-        'Revise onde você precisa melhorar, busque cursinhos preparatórios, e considere outras oportunidades de ingresso, como ENEM ou programas de transferência.').
+        'Revise onde você precisa melhorar, busque cursinhos preparatórios, e considere outras oportunidades de ingresso, como ENEM ou programas de transferência.')
         
